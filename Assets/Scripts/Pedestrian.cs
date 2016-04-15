a34137b588bf65190851035efa0788a417ddf3f0
Alexandros Tsompolis <alekostsom@gmail.com>
2016-04-15 19:56:56 +0300

    Started avoiding static obstacles

--- Assets/Scripts/Pedestrian.cs
+++ Assets/Scripts/Pedestrian.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Pedestrian : MonoBehaviour {
 
@@ -25,6 +26,10 @@ public class Pedestrian : MonoBehaviour {
 
 	float timeIntervel = 5.0f;
 	float distCovered = 0.0f;
+
+	//List of all the objects that are close
+	List<GameObject> closeObjects = new List<GameObject>();
+	public Transform wall;
 
 	void Start () {
 		Init ();
@@ -56,6 +61,14 @@ public class Pedestrian : MonoBehaviour {
 	void Update () {
 		distCovered += Time.deltaTime;
 		transform.position = Vector3.Lerp (curPos, curTargetPos, distCovered/timeIntervel); 
+
+		Debug.Log (Mathf.Abs(wall.position.x - transform.position.x));
+		if (Mathf.Abs (wall.position.x - transform.position.x) < 1.0f) {
+			if ((wall.position.x - transform.position.x) > 0)
+				UpdateSetupX(-10.0f,0.0f);
+			else
+				UpdateSetupX(0.0f,10.0f);
+		}
 	}
 
 	public void UpdateSetup()
@@ -77,6 +90,29 @@ public class Pedestrian : MonoBehaviour {
 		initSpeed = Random.Range (2, 10);
 		timeIntervel = initSpeed;
 		distCovered = 0.0f;		
-		Debug.Log (curPos + " " + curTargetPos + " " + initSpeed + " " + radius);
+		//Debug.Log (curPos + " " + curTargetPos + " " + initSpeed + " " + radius);
+	}
+
+	public void UpdateSetupX(float min, float max)
+	{
+		//yield return new WaitForSeconds(1.0f);
+		/*curPos = new Vector3(Random.Range (-10, 10), transform.position.y, transform.position.z);
+		Debug.Log (curPos);
+		transform.position = curPos;*/
+		//StartCoroutine (UpdateSetup ());
+		curPos = transform.position;
+		
+		finalTargetPos.x = Random.Range (min, max);
+		finalTargetPos.y = 0;
+		finalTargetPos.z = 0;
+		//finalTargetPos.z = Random.Range (-10, 10);
+		finalTargetPos += curPos;
+		curTargetPos = finalTargetPos;
+		//Debug.Log (curPos + " " + curTargetPos);
+		
+		initSpeed = Random.Range (2, 10);
+		timeIntervel = initSpeed;
+		distCovered = 0.0f;		
+		//Debug.Log (curPos + " " + curTargetPos + " " + initSpeed + " " + radius);
 	}
 }
